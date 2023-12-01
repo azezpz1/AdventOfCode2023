b1daa4ca68975821b402edf2b6df6b8a670c60c0
Anthony Panettiere <apanettiere@fastmail.com>
2023-12-01 06:34:40 -0600

    Add real calibration data and calculate answer to part 1

--- AdventOfCode2023/main.py
+++ AdventOfCode2023/main.py
@@ -1,6 +1,22 @@
-def add(a: int, b: int) -> int:
-    """
-    >>> add(1, 2)
-    3
-    """
-    return a + b
+from pathlib import Path
+
+import calibration_line_decoder
+
+
+def main():
+    total = get_calibration_amount()
+    print(total)
+
+
+def get_calibration_amount():
+    directory = Path(__file__).parent.absolute()
+    calibration_data_path = directory / "inputs" / "calibration_data.txt"
+    with open(calibration_data_path) as f:
+        total = 0
+        for line in f:
+            total += calibration_line_decoder.decode_line(line)
+    return total
+
+
+if __name__ == "__main__":
+    main()
