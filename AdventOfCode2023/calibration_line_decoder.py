119c1417e4fb5e788feec2837f442b2a049b5e1a
Anthony Panettiere <apanettiere@fastmail.com>
2023-12-02 09:49:38 -0600

    Handle numbers that are words

--- AdventOfCode2023/calibration_line_decoder.py
+++ AdventOfCode2023/calibration_line_decoder.py
@@ -19,21 +19,68 @@ def decode_line(line: str) -> int:
     13
     >>> decode_line("xtwone3four")
     24
-    >>> decode_line('4nineeightseven2")
+    >>> decode_line("4nineeightseven2")
     42
     >>> decode_line("zoneight234")
     14
     >>> decode_line("7pqrstsixteen")
     76
     """
-    digits = get_first_digit(line) + get_first_digit(line, True)
-    return int(digits)
+    first = ""
+    last = ""
+    index_of_first = 0
+    index_of_last = 0
+
+    index = 0
+    for index in range(len(line)):
+        number_at_index = get_number_from_index(index, line)
+        if first == "":
+            first = number_at_index
+            index_of_first = index
+        if number_at_index != "":
+            last = number_at_index
+            index_of_last = index
+
+    return int(first + last)
 
 
-def get_first_digit(line: str, is_reversed=False) -> str:
-    if is_reversed:
-        line = line[::-1]
-    for char in line:
-        if char.isdigit():
-            return char
+def get_number_from_index(index: int, line: str) -> str:
+    """Gets the number (either because it's a digit or the word) from the given index
+
+    Args:
+        index (int): The index to check for the number at.
+        line (str): The string that contains numbers
+
+    Returns:
+        str: A string representation of the number that was found (e.g. "1", "2", etc.)
+
+    >>> get_number_from_index(0, "123")
+    '1'
+    >>> get_number_from_index(1, "123")
+    '2'
+    >>> get_number_from_index(0, "a12")
+    ''
+    >>> get_number_from_index(0, "one")
+    '1'
+    >>> get_number_from_index(3, "onetwo")
+    '2'
+    """
+    possible_number_strings = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+    }
+    if line[index].isdigit():
+        return line[index]
+    else:
+        for key, value in possible_number_strings.items():
+            potential_number_string = line[index : index + len(key)]
+            if key == potential_number_string:
+                return value
     return ""
